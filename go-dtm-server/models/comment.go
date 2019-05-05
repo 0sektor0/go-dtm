627dd14167c9914d879dabf052ef20b17a802369
0sekto0 <tomskiy_a@list.ru>
2019-05-05 19:31:40 +0300

    extra files deleted

--- go-dtm-server/models/comment.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Comment struct {
-	Id          int  	`json:"id"`
-	Developer   *User  	`json:"developer"`
-	DeveloperId int  	`json:"developerId"`
-	Text        string 	`json:"text"`
-}
--- go-dtm-server/models/user.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type User struct {
-	Id       int    `json:"id"`
-	Login    string `json:"login"`
-	Picture  string `json:"picture"`
-	IsAdmin  bool   `json:"isAdmin"`
-	Password string `json:"-"`
-}
